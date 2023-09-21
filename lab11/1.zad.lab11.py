78b3eef9755fbac88937bb80cfdc8240fd0af0a9
stanimarinov <stani.marinov@yahoo.com>
2023-09-21 17:14:21 +0300

    add zad

---
+++ lab11/1.zad.lab11.py
@@ -0,0 +1,7 @@
+#Задача 1. Да се включи time module и да се извика функцията sleep. Накарайте 
+#програмата да заспи за 5 секунди и тогава принтирайте “Time module imported”. 
+
+
+import time
+print(time.sleep(5))
+print("Time module imported")
---
+++ lab11/2.zad.lab11/calculation.py
@@ -0,0 +1,5 @@
+def calc_area(length,width):
+    length = int(input("Enter length: "))
+    width = int(input("Enter width: "))
+    calc_area = length*width
+    print(calc_area)
